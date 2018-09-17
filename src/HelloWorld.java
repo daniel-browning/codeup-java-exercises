88a787841f9960077160fe052ccb01924bacf627
daniel-browning <daniel.d.browning@gmail.com>
2018-09-17 14:13:47 -0500

    updating for Syntax, Types, and Variables exercise;

---
+++ src/HelloWorld.java
@@ -0,0 +1,35 @@
+public class HelloWorld {
+        public static void main(String[] args) {
+            System.out.print("Hello, ");
+            System.out.println("World!");
+
+            int myFavoriteNumber = 3;
+            System.out.println(myFavoriteNumber);
+
+            String myString = "Dachshund";
+            System.out.println(myString);
+            float myNumber = 3;
+            System.out.println(myNumber);
+
+//            int x = 5;
+//            System.out.println(x++);
+//            System.out.println(x);
+
+//            int x = 5;
+//            System.out.println(++x);
+//            System.out.println(x);
+
+//            int y = 4;
+//            System.out.println(y += 5);
+
+//            int x = 3;
+//            int y = 4;
+//            System.out.println(x *= y);
+
+            int x = 10;
+            int y = 2;
+            System.out.println(x /= y);
+            System.out.println(y -= x);
+    }
+}
+
