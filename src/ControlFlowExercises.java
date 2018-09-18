eb4f2acf90d26ca3d688c1ea03fe8768985806a5
daniel-browning <daniel.d.browning@gmail.com>
2018-09-18 15:19:43 -0500

    pushing post exercise 1 and before exercise 2 (Fizzbuzz);

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,15 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
+for (int i = 5; i <= 15; i++) {
+    System.out.print(i + " ");
+}
 
+for (int n = 100; n >= -10; n -= 5) {
+    System.out.println(n + " ");
+}
+
+for (long x = 2; x < 1000000; x *= x) {
+    System.out.println(x * x + " ");
+}
     }
 }
